fe608482f84f3fc923d7dc9f15b7f01febed43dd
htorkashvand <facebook.hamedtarkashvand@gmail.com>
2022-04-03 18:01:31 +0430

    config  responsive

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './index.scss';
 import RoutesApp from './routes';
 import reportWebVitals from './reportWebVitals';
 import store from './Redux/store';
